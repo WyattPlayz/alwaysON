4506679fb441a6cbfffb65a51e806793391a76d9
Wyatt Lipscomb <35582368+WyattPlayz@users.noreply.github.com>
2019-03-04 11:06:28 -0500

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,10 @@
+@echo off
+title AlwaysON installer
+cls
+echo Please press any key to install required dependices.
+pause;
+npm install got
+npm install node-key-sender
+echo Install Complete.
+pause;
+exit
